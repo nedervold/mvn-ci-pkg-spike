0f40ddc3b09e79d45710010fafb51e5e6e72b209
Eric Nedervold <nedervoldsoftware@gmail.com>
2020-05-09 12:53:01 -0700

    Fixed typo, removed saving artifacts.

--- src/main/java/org/nedervold/mvncipkgspike/App.java
+++ src/main/java/org/nedervold/mvncipkgspike/App.java
@@ -2,7 +2,7 @@ package org.nedervold.mvncipkgspike;
 
 public class App {
   public void service() {
-    System.out.println("Howdy, universe, from" + System.getProperty("os.name") + "!");
+    System.out.println("Howdy, universe, from " + System.getProperty("os.name") + "!");
   }
 
   public static void main(String[] args) {
